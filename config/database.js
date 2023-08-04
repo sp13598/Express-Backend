ded6855b03af1230d3d6ebcfc1ccaeb48bec0b5f
Shubham Patil <113597449+sp13598@users.noreply.github.com>
2023-08-04 15:28:25 +0530

    Delete config directory

--- config/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('bank2', 'root', 'Pass@123', {
-  host: 'localhost',
-  dialect: 'mysql',
-});
-
-module.exports = sequelize;
--- config/dbConfig.js
+++
@@ -1,7 +0,0 @@
-module.exports ={
-    HOST: 'localhost',
-    USER: 'root',
-    PASSWORD: 'Pass@123',
-    DB: 'bank',
-    dialect: 'mysql'
-}
